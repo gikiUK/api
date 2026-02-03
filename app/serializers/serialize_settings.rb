323eb181f184c40249f005c665df3d92b4436076
Jeremy Walker <jez.walker@gmail.com>
2026-02-03 10:38:45 +0000

    Update app/serializers/serialize_settings.rb

--- app/serializers/serialize_settings.rb
+++ app/serializers/serialize_settings.rb
@@ -6,8 +6,8 @@ class SerializeSettings
   def call
     {
       locale: user.locale,
-      receive_newsletters: user.data.receive_newsletters,
-      notifications_enabled: user.data.notifications_enabled
+      receive_newsletters: user.receive_newsletters?,
+      notifications_enabled: user.notifications_enabled?
     }
   end
 end
